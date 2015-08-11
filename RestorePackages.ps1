f8c8560b27a1fa689cbd2fd9c230da75276480bf
Peter Dunshee <peter.dunshee@smartthings.com>
2015-08-11 13:54:19 -0500

    env variable workaround

---
+++ RestorePackages.ps1
@@ -0,0 +1,18 @@
+﻿# Save current settings prior to changing
+$currentConfig = $env:Configuration
+$currentPlatform = $env:Platform
+
+# Workaround for nuget 3.1 weirdness:
+# These need to be default, otherwise
+# Nuget will try to assert things based
+# on the build configuration.  Since
+# some configurations don't exist in some
+# projects (by design), this causes it to
+# fail.
+$env:Configuration = ''
+$env:Platform = ''
+nuget restore
+
+# Restore
+$env:Configuration = $currentConfig
+$env:Platform = $currentPlatform
